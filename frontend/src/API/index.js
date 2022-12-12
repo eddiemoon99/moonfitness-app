ce43988a1d6090f28c92e812d8bb0e5d2631f05a
Eddie Moon <eddiemoon@eddies-mbp.home>
2022-12-12 03:14:55 -0500

    change api link

--- frontend/src/API/index.js
+++ frontend/src/API/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // create axios instance
-const API = axios.create({ baseURL: 'http://localhost:8000' });
+const API = axios.create({ baseURL: 'https://moonfitness-api.herokuapp.com' });
 
 // middleware send auth user to backend
 API.interceptors.request.use((req) => {
